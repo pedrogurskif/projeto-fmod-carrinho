a2f789ed2a2da073870adbb0f47d47a889bfbb83
Pedro <pedrogurskif@gmail.com>
2026-05-26 17:16:34 -0300

    save

--- CarrinhoUNITY/Assets/Scripts/PlayerController.cs
+++ CarrinhoUNITY/Assets/Scripts/PlayerController.cs
@@ -5,11 +5,13 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 20f;
+    private float speed = 10f;
     private float turnSpeed = 75f;
     [SerializeField] private InputActionAsset inputActions;
     private InputAction moveAction;
     private Vector2 playerInput;
+    private Vector3 oldPosition;
+    public float velocidade = 0f;
 
     void Start()
     {
@@ -18,6 +20,9 @@ public class PlayerController : MonoBehaviour
     
     void Update()
     {
+        velocidade = ((transform.position - oldPosition).magnitude) / Time.deltaTime;
+        oldPosition = transform.position;
+
         playerInput = moveAction.ReadValue<Vector2>();
         transform.Translate(Vector3.forward * Time.deltaTime * speed * playerInput.y);
         transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * playerInput.x);
